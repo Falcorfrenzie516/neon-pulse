1a04e3a4868a4b493956c832f2e6d6ca3a3d1a70
Falcorfrenzie516 <84740784+Falcorfrenzie516@users.noreply.github.com>
2026-08-10 12:48:23 -0400

    Update endRun.js

--- generators/endRun.js
+++ generators/endRun.js
@@ -3,82 +3,100 @@
   Alpha 1.9.x
 
   File location:
-  text/EndRun.js
+  text/endRun.js
 */
 
 window.EndRunText = (() => {
-  const titles = [
-    'TOUR CANCELLED'
-  ];
+  const title = 'TOUR CANCELLED';
 
-  const reasons = [
-    'Hydration Error',
-    'Glow Stick Incident',
-    'Unplanned Stage Exit',
-    'The Drop Dropped You',
-    '"One More..."'
+  const deaths = [
+    {
+      title: 'Hydration Error',
+      body: 'You forgot to drink water. Again.<br>Somewhere between “one more song” and “I’m totally fine,” your body submitted a formal resignation.',
+      punch: 'Tour cancelled. Hydrate next build.'
+    },
+    {
+      title: 'Glow Stick Incident',
+      body: 'You stared into the spinning glow sticks for a little too long.<br>They were pretty. They were mesmerizing. They were apparently stronger than your sense of direction.',
+      punch: 'You have no idea where you are. Neither does the tour bus.'
+    },
+    {
+      title: 'Unplanned Stage Exit',
+      body: 'You danced a little too hard and accidentally launched yourself off the stage.<br>The crowd thought it was part of the show.',
+      punch: 'It was not part of the show.'
+    },
+    {
+      title: 'The Drop Dropped You',
+      body: 'You spent all night waiting for the perfect drop.<br>It finally came. So did your last remaining ounce of energy.',
+      punch: 'The beat survived. You did not.'
+    },
+    {
+      title: 'One More Song',
+      body: 'You said, “One more song.”<br>Then you said it six more times.<br>The venue turned the lights on. The staff went home. Someone is vacuuming around you.',
+      punch: 'Take the hint. The tour is over.'
+    }
   ];
 
   const replayButtons = [
-    'PLAY IT BACK',
-    'RUN IT AGAIN',
-    'ONE MORE SET',
-    'BACK ON STAGE',
-    'RELOAD THE SET',
-    'QUEUE IT UP',
-    'ANOTHER ROUND',
-    'DROP IT AGAIN'
+    'PLAY IT BACK'
   ];
 
-  const restButtons = [
+  const exitButtons = [
     'GET SOME REST',
+    'BACKSTAGE',
+    'BACK TO THE GREEN ROOM',
+    'LEAVE THE DECKS',
     'CALL IT A NIGHT',
-    'LEAVE THE VENUE',
-    'HEAD BACKSTAGE',
-    'CLOCK OUT',
-    'PACK IT UP',
+    'END THE AFTERS',
+    'PACK UP THE GEAR',
+    'POWER DOWN',
+    'CLOCK OUT, DJ',
+    'EXIT THE BOOTH',
+    'LEAVE THE RAVE',
+    'SAVE YOUR EARS',
+    'TOUR BUS TIME',
+    'FIND THE TOUR BUS',
     'GO TOUCH GRASS',
-    'SAVE YOUR HEARING'
+    'HYDRATE & DISAPPEAR',
+    'LOG OFF THE DECKS',
+    '> RETURN: BACKSTAGE',
+    '> DJ.STATUS = OFFLINE',
+    'GO BE A PERSON'
   ];
 
   function randomFrom(list) {
     return list[Math.floor(Math.random() * list.length)];
   }
 
-  function getTitle() {
-    return randomFrom(titles);
-  }
-
-  function getReason() {
-    return randomFrom(reasons);
+  function getDeath() {
+    return randomFrom(deaths);
   }
 
   function getReplayButton() {
     return randomFrom(replayButtons);
   }
 
-  function getRestButton() {
-    return randomFrom(restButtons);
+  function getExitButton() {
+    return randomFrom(exitButtons);
   }
 
   function getEndRunText() {
     return {
-      title: getTitle(),
-      reason: getReason(),
+      title,
+      death: getDeath(),
       replayButton: getReplayButton(),
-      restButton: getRestButton()
+      exitButton: getExitButton()
     };
   }
 
   return {
-    titles,
-    reasons,
+    title,
+    deaths,
     replayButtons,
-    restButtons,
-    getTitle,
-    getReason,
+    exitButtons,
+    getDeath,
     getReplayButton,
-    getRestButton,
+    getExitButton,
     getEndRunText
   };
 })();
